c73835dd4aec4355b3b6583554426940b4e7c206
zhuiUnity <34669462+zhuiUnity@users.noreply.github.com>
2017-12-19 20:20:24 +0800

    2017/12/19
    添加10行代码

--- Assets/Script/1.cs
+++ Assets/Script/1.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("????????????????????????????");
-	}
+	void OnEnable()
+    {
+        Debug.Log("1");
+        Debug.Log("2");
+        Debug.Log("3");
+        Debug.Log("4");
+        Debug.Log("5");
+        Debug.Log("6");
+        Debug.Log("7");
+        Debug.Log("8");
+        Debug.Log("9");
+        Debug.Log("10");
+    }
 	
 	// Update is called once per frame
 	void Update () {
